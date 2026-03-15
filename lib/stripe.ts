0d7fc6a4d06d460a60e7b31415880422396452c2
tawfikyassine <herr.tawfik@gmail.com>
2026-03-15 09:46:10 +0100

    fix: update Stripe API version to 2026-02-25.clover

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -3,6 +3,6 @@ import Stripe from 'stripe'
 export const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY || 'sk_test_dummy',
   {
-    apiVersion: '2025-02-24.acacia',
+    apiVersion: '2026-02-25.clover',
   }
 )
